bed5f6bb531775c0ad4c9890ce0b97ca6e01f117
Szymon Nachlik <72997724+Chemafiz@users.noreply.github.com>
2022-05-16 22:56:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import os
-import time
 import numpy as np
+import copy
+import time
+import os
 
 
 class GameOfLife:
+    '''Game of life class'''
     def __init__(self, N, start_size, iterations_number):
         self.N = N
         self.iterations_number = iterations_number
@@ -13,43 +15,47 @@ class GameOfLife:
         self.table[index:(index + start_size), index:(index + start_size)] = 1
 
 
-    def evolution(self):
-        for i in range(self.iterations_number):
-            for i, row in enumerate(self.table):
-                for j, element in enumerate(row):
-                    if element == 0 and self.neighbors_counter((i,j)) == 3:
-                        self.table[i,j] = 1
-                    elif not (element == 1 and (2 <= self.neighbors_counter((i,j)) <= 3)):
-                        self.table[i,j] = 0
-            time.sleep(0.05)
-            if os.name =="nt":
-                os.system("cls")
-            else:
-                os.system("clear")
-            self.print_state(".")
-                        
-
     def print_state(self, symbol):
         print("_"*self.N*2)
         for row in self.table:
             print("|", end="")
             print(" ".join(list(map(lambda x: " " if x==0 else symbol, row))), end="")
             print("|")
-        print("_"*self.N*2)
+        print("_" * self.N*2)
 
+    def evolution(self):
+        for _ in range(self.iterations_number):
+            kopia = copy.deepcopy(self.table)
+            self.print_state("#")
+            for i, row in enumerate(kopia):
+                for j, element in enumerate(row):
+                    if element == 0 and self.neighbors_counter(kopia, (i,j)) == 3:
+                       self.table[i,j] = 1
+                    elif element == 1 and self.neighbors_counter(kopia, (i,j)) != 2 and self.neighbors_counter(kopia, (i,j)) != 3:
+                        self.table[i,j] = 0 
+            time.sleep(0.2)
+            if os.name =="nt":
+                os.system("cls")
+            else:
+                os.system("clear")
+                
+            
 
-    def neighbors_counter(self, position):
-        counter = self.table[position[0] - 1, position[1] - 1] + \
-                  self.table[position[0] - 1, position[1]] + \
-                  self.table[position[0] - 1, (position[1] + 1) % self.N]    
+    def neighbors_counter(self,kopia, position):
+        counter = kopia[position[0] - 1, position[1] - 1] + \
+                  kopia[position[0] - 1, position[1]] + \
+                  kopia[position[0] - 1, (position[1] + 1) % self.N]
+        
+        counter += kopia[position[0], position[1] - 1] + \
+                   kopia[position[0], (position[1] + 1) % self.N]
 
-        counter += self.table[position[0], position[1] - 1] + self.table[position[0], (position[1] + 1) % self.N]
-
-        counter += self.table[(position[0] + 1) % self.N, position[1] - 1] + \
-                   self.table[(position[0] + 1) % self.N, position[1]] + \
-                   self.table[(position[0] + 1) % self.N, (position[1] + 1) % self.N] 
+        counter += kopia[(position[0] + 1) % self.N, position[1] - 1] + \
+                   kopia[(position[0] + 1) % self.N, position[1]] + \
+                   kopia[(position[0] + 1) % self.N, (position[1] + 1) % self.N] 
         return counter
 
+
+
 if __name__ == "__main__":
-    game= GameOfLife(30, 9, 1000)
+    game= GameOfLife(30, 11, 2000)
     game.evolution()
